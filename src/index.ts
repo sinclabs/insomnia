a7f9974b1ad940e56fc896e7ccd5d6254f12c9bf
Subramanian Alagappan <9549230+sinclabs@users.noreply.github.com>
2024-03-22 10:22:05 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,42 @@
-import { createEntity } from "./aframe";
-import { 
-    getRandomArbitrary,
-    getRandomInt 
-} from "./utils";
+import { createEntity } from "./aframe"
+import { getRandomArbitrary, getRandomInt } from "./utils"
 
 function createRandomCylinders() {
-    const numberOfCylinders = getRandomInt(50, 100)
-    const cylinders: Array<{
-        name: "a-cylinder",
-        components: {
-            [name: string]: any
-        }
-    }> = []
-
-    for(let i=0; i<100; i++) {
-        cylinders.push({
-            name: "a-cylinder",
-            components: {
-                position: `${getRandomArbitrary(-40, 40)} ${getRandomArbitrary(-0.5, 0.5)} ${getRandomArbitrary(-40, 40)}`,
-                radius: getRandomArbitrary(1, 1.5),
-                height: getRandomArbitrary(30, 50),
-                color: "#FFC65D",
-                shadow: true
-            }
-        })
+  const numberOfCylinders = getRandomInt(50, 100)
+  const cylinders: Array<{
+    name: "a-cylinder"
+    components: {
+      [name: string]: any
     }
+  }> = []
 
-    return cylinders
+  for (let i = 0; i < numberOfCylinders; i++) {
+    cylinders.push({
+      name: "a-cylinder",
+      components: {
+        position: `${getRandomArbitrary(-40, 40)} ${getRandomArbitrary(
+          -0.5,
+          0.5
+        )} ${getRandomArbitrary(-40, 40)}`,
+        radius: getRandomArbitrary(1, 1.5),
+        height: getRandomArbitrary(30, 50),
+        color: "#FFC65D",
+        shadow: true,
+      },
+    })
+  }
+
+  return cylinders
 }
 
-(function main() {
-    const scene = document.getElementsByTagName('a-scene')[0]
+;(function main() {
+  const scene = document.getElementsByTagName("a-scene")[0]
 
-    scene.setAttribute("background", "color: #3b3b3b")
-    
-    const elementsToAppend = createRandomCylinders().map(({ name, components }) => createEntity(name, components))
+  scene.setAttribute("background", "color: #3b3b3b")
 
-    elementsToAppend.forEach(element => scene.appendChild(element))
+  //   const elementsToAppend = createRandomCylinders().map(({ name, components }) =>
+  //     createEntity(name, components)
+  //   )
+
+  //   elementsToAppend.forEach((element) => scene.appendChild(element))
 })()
